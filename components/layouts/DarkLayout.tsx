9edd74cf38eae280039ee1b7435b8d905a276da5
Walter Gomez <waltherfgs@gmail.com>
2022-07-13 18:44:49 -0500

    pre-deployment vercel

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import { FC, PropsWithChildren } from "react";
+
+export const DarkLayout: FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<div
+			style={{
+				backgroundColor: "rgba(0,0,0,0.3)",
+				padding: "10px",
+				borderRadius: "5px",
+			}}
+		>
+			<h3>Dark Layout</h3>
+
+			<div>{children}</div>
+		</div>
+	);
+};
