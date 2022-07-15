0afb983d1154531ce14f1d28fa1c0c8f9f949e46
Walter Gomez <waltherfgs@gmail.com>
2022-07-15 11:55:41 -0500

    docker

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	output: "standalone",
+};
+
+module.exports = nextConfig;
